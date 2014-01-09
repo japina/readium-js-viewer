f1c996f9ab90ca0967a8bf4f499ab5137358b32f
Boštjan Jerko <bostjan@japina.eu>
2014-01-09 08:45:25 +0100

    ditching iframe

---
+++ index.js
@@ -0,0 +1,24 @@
+function initDialog(){
+	$( "#epubdialog" ).dialog({
+		dialogClass: "no-close",
+/*	buttons: [
+		{
+			text: "Exit",
+				click: function() {
+					$( this ).dialog( "close" );
+				}
+			}
+		], */
+		width: 400,
+		height: 800,
+		autoOpen: false
+	});
+
+	console.log("init finished");
+}
+
+function openDialog(){
+	$("#epubdialog").dialog("open");
+}
+
+$(document).ready(initDialog());
